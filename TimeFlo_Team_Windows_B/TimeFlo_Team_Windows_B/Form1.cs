fb7f42c35515b192289cd866dd68f3eceb092b55
Zachary Springer <zachar7@cecs.pdx.edu>
2021-12-06 16:38:02 -0800

    Added beep sound, added khodaks button dimming and start/pause button fix, added session counter.

--- TimeFlo_Team_Windows_B/TimeFlo_Team_Windows_B/Form1.cs
+++ TimeFlo_Team_Windows_B/TimeFlo_Team_Windows_B/Form1.cs
@@ -1,9 +1,12 @@
+using System.Media;
 namespace TimeFlo_Team_Windows_B
 {
     public partial class Form1 : Form
     {
         private int totalSecondsWork;
         private int totalSecondsBreak;
+        private bool paused;
+        private int workSessions;
         public Form1()
         {
             InitializeComponent();
@@ -16,12 +19,26 @@ namespace TimeFlo_Team_Windows_B
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int minutes = int.Parse(this.numericUpDown1.Value.ToString());
-            totalSecondsWork = minutes * 60;
-            int breakmin = int.Parse(this.numericUpDown2.Value.ToString());
-            totalSecondsBreak = breakmin * 60;
+            button1.Enabled = false;
+            button2.Enabled = true;
+            button3.Enabled = true;
 
-            this.timer1.Enabled = true;
+            if (paused == true)
+            {
+                this.timer1.Enabled = true;
+                paused = false;
+            }
+            else
+            {
+                int minutes = int.Parse(this.numericUpDown1.Value.ToString());
+                totalSecondsWork = minutes * 60;
+                int breakmin = int.Parse(this.numericUpDown2.Value.ToString());
+                totalSecondsBreak = breakmin * 60;
+                ++workSessions;
+                this.label5.Text = workSessions.ToString();
+
+                this.timer1.Enabled = true;
+            }
         }
 
         private void numericUpDown1_ValueChanged(object sender, EventArgs e)
@@ -41,13 +58,20 @@ namespace TimeFlo_Team_Windows_B
 
         private void button2_Click(object sender, EventArgs e)
         {
+            this.button2.Enabled = false;
+            this.button1.Enabled = true;
+
             this.timer1.Enabled = false;
+            paused = true;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             this.timer1.Stop();
             this.label1.Text = "00:00";
+            this.button1.Enabled = true;
+            this.button2.Enabled = true;
+            paused = false;
         }
 
         private void label5_Click(object sender, EventArgs e)
@@ -62,18 +86,47 @@ namespace TimeFlo_Team_Windows_B
                 totalSecondsWork--;
                 int minutes = totalSecondsWork / 60;
                 int seconds = totalSecondsWork - (minutes * 60);
-                this.label1.Text = minutes.ToString() + ":" + seconds.ToString();
+                //this.label1.Text = minutes.ToString() + ":" + seconds.ToString();
+                if (minutes < 10 && seconds < 10)
+                    this.label1.Text = "0" + minutes.ToString() + ":0" + seconds.ToString();
+                else if (minutes < 10 && seconds > 10)
+                    this.label1.Text = "0" + minutes.ToString() + ":" + seconds.ToString();
+                else if (minutes > 10 && seconds < 10)
+                    this.label1.Text = minutes.ToString() + ":0" + seconds.ToString();
+                else
+                    this.label1.Text = minutes.ToString() + ":" + seconds.ToString();
+
+            }
+            else if (totalSecondsWork == 0)
+            {
+                SystemSounds.Beep.Play();
+                totalSecondsWork = -1;
             }
             else if(totalSecondsBreak > 0)
             {
                 totalSecondsBreak--;
                 int breakmin = totalSecondsBreak / 60;
                 int breaksec = totalSecondsBreak - (breakmin * 60);
-                this.label1.Text = breakmin.ToString() + ":" + breaksec.ToString();
+                //this.label1.Text = breakmin.ToString() + ":" + breaksec.ToString();
+                if (breakmin < 10 && breaksec < 10)
+                    this.label1.Text = "0" + breakmin.ToString() + ":0" + breaksec.ToString();
+                else if (breakmin < 10 && breaksec > 10)
+                    this.label1.Text = "0" + breakmin.ToString() + ":" + breaksec.ToString();
+                else if (breakmin > 10 && breaksec < 10)
+                    this.label1.Text = breakmin.ToString() + ":0" + breaksec.ToString();
+                else
+                    this.label1.Text = breakmin.ToString() + ":" + breaksec.ToString();
+
+            }
+            else if (totalSecondsBreak == 0)
+            {
+                SystemSounds.Beep.Play();
+                totalSecondsBreak = -1;
             }
             else
             {
                 this.timer1.Stop();
+                paused = false;
             }
         }
     }
