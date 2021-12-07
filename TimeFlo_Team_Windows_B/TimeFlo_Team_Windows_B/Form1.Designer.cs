1920c5f4ab020b2def3bbcead529407c4dacb106
Elspeth Anderson <elspet@pdx.edu>
2021-12-06 19:10:58 -0800

    Defined upper/lower bounds of work/break time selectors, updated formatting

--- TimeFlo_Team_Windows_B/TimeFlo_Team_Windows_B/Form1.Designer.cs
+++ TimeFlo_Team_Windows_B/TimeFlo_Team_Windows_B/Form1.Designer.cs
@@ -46,10 +46,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(57, 142);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(66, 221);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(71, 26);
+            this.button1.Size = new System.Drawing.Size(101, 43);
             this.button1.TabIndex = 0;
             this.button1.Text = "START";
             this.button1.UseVisualStyleBackColor = true;
@@ -57,10 +56,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(132, 142);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Location = new System.Drawing.Point(174, 221);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(72, 26);
+            this.button2.Size = new System.Drawing.Size(103, 43);
             this.button2.TabIndex = 1;
             this.button2.Text = "PAUSE";
             this.button2.UseVisualStyleBackColor = true;
@@ -68,10 +66,19 @@
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(145, 184);
-            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.numericUpDown1.Location = new System.Drawing.Point(192, 291);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(126, 23);
+            this.numericUpDown1.Size = new System.Drawing.Size(180, 31);
             this.numericUpDown1.TabIndex = 2;
             this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.numericUpDown1.Value = new decimal(new int[] {
@@ -83,10 +90,19 @@
             // 
             // numericUpDown2
             // 
-            this.numericUpDown2.Location = new System.Drawing.Point(146, 220);
-            this.numericUpDown2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.numericUpDown2.Location = new System.Drawing.Point(194, 351);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
             this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(126, 23);
+            this.numericUpDown2.Size = new System.Drawing.Size(180, 31);
             this.numericUpDown2.TabIndex = 3;
             this.numericUpDown2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.numericUpDown2.Value = new decimal(new int[] {
@@ -99,20 +115,18 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(56, 184);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(65, 291);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(84, 15);
+            this.label2.Size = new System.Drawing.Size(126, 25);
             this.label2.TabIndex = 5;
             this.label2.Text = "Work Minutes:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(57, 222);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(66, 354);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(85, 15);
+            this.label3.Size = new System.Drawing.Size(127, 25);
             this.label3.TabIndex = 6;
             this.label3.Text = "Break Minutes:";
             // 
@@ -120,10 +134,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(17, 15);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(283, 128);
+            this.label1.Size = new System.Drawing.Size(423, 191);
             this.label1.TabIndex = 7;
             this.label1.Text = "00:00";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -132,10 +145,9 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(105, 253);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(155, 406);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(96, 15);
+            this.label4.Size = new System.Drawing.Size(146, 25);
             this.label4.TabIndex = 8;
             this.label4.Text = "Session Number:";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -143,10 +155,9 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(145, 268);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Location = new System.Drawing.Point(212, 431);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(13, 15);
+            this.label5.Size = new System.Drawing.Size(22, 25);
             this.label5.TabIndex = 9;
             this.label5.Text = "1";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -154,10 +165,9 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(208, 142);
-            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Location = new System.Drawing.Point(282, 221);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(66, 26);
+            this.button3.Size = new System.Drawing.Size(94, 43);
             this.button3.TabIndex = 10;
             this.button3.Text = "RESET";
             this.button3.UseVisualStyleBackColor = true;
@@ -170,9 +180,9 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(306, 292);
+            this.ClientSize = new System.Drawing.Size(443, 477);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -183,7 +193,6 @@
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "TimeFlo";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
